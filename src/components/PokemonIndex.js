8f8c4d99ea055a3cb714097d1af4d14b38b5898c
byronsk <vyskoutaris@hotmail.com>
2019-04-30 10:56:14 +0100

    Done

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,19 +5,45 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    searchTerm: ''
+  }
+
+  updateSearchTerm = searchTerm => {
+    this.setState({
+      searchTerm: searchTerm
+    })
+  }
+
+  filteredPokemon = () => {
+    return this.state.pokemons.filter(pokemon =>
+    pokemon.name.includes(this.state.searchTerm))
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
-        <br />
-        <PokemonCollection />
+        <Search onSearchChange={event => this.updateSearchTerm(event.target.value)} showNoResults={false} />
         <br />
         <PokemonForm />
+        <br />
+        <PokemonCollection pokemon={this.filteredPokemon()} />
       </div>
     )
   }
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(response => response.json())
+    .then(pokemons => {
+      this.setState({
+        pokemons: pokemons
+      })
+    })
+  }
+
 }
 
 export default PokemonPage
